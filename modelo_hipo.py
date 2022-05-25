ba59317e7468d316f16347680d488e87767a3f05
Cesar Zapata <cesarzr10@gmail.com>
2022-05-25 15:17:24 -0500

    Implementando modelo mixto y organizando el código

--- modelo_hipo.py
+++ modelo_hipo.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 # función para hallar valores de la siguiente generación
 def N_tn(I0, J0, A0, M0, control): # nit = número de iteraciones/generaciones
 
-    castracion = (control == 'C' or control == 'CS') # (booleano) checkea si el mecanismo de control introducido es castración
-    sacrificio = (control == 'S' or control == 'CS') # (booleano) checkea si el mecanismo de control introducido es sacrificio
+    castracion = (control == 'C' or control == 'M') # (booleano) checkea si el mecanismo de control introducido es castración o mixto
+    sacrificio = (control == 'S' or control == 'M') # (booleano) checkea si el mecanismo de control introducido es sacrificio o mixto
 
     alpha = 1*0.5*0.8  # tasa de reproducción
     mI = 0.295 # tasa mort años 0-1
@@ -20,7 +20,6 @@ def N_tn(I0, J0, A0, M0, control): # nit = número de iteraciones/generaciones
     aI = 1-mI-pI #porcentaje avance grupo de edad #http://www.conservationecologylab.com/uploads/1/9/7/6/19763887/lewison_2007.pdf
     aJ = 1-mJ-pJ
     aA = 1-mJ-pA
-    aM = 1-mJ-pM
 
     # si se usa la castracion como mecanismo de control -> tasa de reproducción se ve reducida
     if castracion:
@@ -28,7 +27,7 @@ def N_tn(I0, J0, A0, M0, control): # nit = número de iteraciones/generaciones
 
     I = np.eye(4, 4) # matriz identidad
     Nt = np.vstack(np.array([I0, J0, A0, M0])) # vector de distribución de edades
-    N = np.sum(Nt) 
+    N = np.sum(Nt) # sumatoria de vector de distibución de edades
 
     Leslie = np.array([[  pI,    0, alpha*pA,  0], # matriz de Lefkovich #http://matema.ujaen.es/jnavas/web_recursos/archivos/matriciales/modelos%20matriciales%20tablas%20vida%20leslie.pdf
                        [  aI,    pJ,    0,     0],
@@ -36,7 +35,7 @@ def N_tn(I0, J0, A0, M0, control): # nit = número de iteraciones/generaciones
                        [  0 ,    0 ,    aA,   pM]])
 
 
-    # si se usa el sacrificio, se saca n número de hipopótamos por año (cte)
+    # si se usa el sacrificio, se saca n número de hipopótamos de todas las categorías por año (cte)
     if sacrificio:
         next_it = Nt+((K-N) / K) * np.dot((Leslie- I), (Nt)) # solo para la siguiente generación
         arr = np.around(next_it,0) # cogiendo el único vector de la matriz que da algún número
@@ -49,20 +48,21 @@ def N_tn(I0, J0, A0, M0, control): # nit = número de iteraciones/generaciones
         return final_arr
 
 
-    next_it = Nt+((K-N) / K) * np.dot((Leslie- I), (Nt))# solo para la siguiente generación
+    next_it = Nt+((K-N) / K) * np.dot((Leslie- I), (Nt)) # solo para la siguiente generación
 
-    arr = np.around(next_it,0)# cogiendo el único vector de la matriz que da algún número
+    arr = np.around(next_it,0) # cogiendo el único vector de la matriz que da algún número
     return arr
 
 
+# función para graficar el crecimiento poblacional
 def graph(n_iter, I0, J0, A0, M0):
     I, J, A , M = np.ones(1)*I0, np.ones(1)*J0, np.ones(1)*A0, np.ones(1)*M0
-    Io, Jo, Ao, Mo = I0, J0, A0, M0
+    Io, Jo, Ao, Mo = I0, J0, A0, M0 # guadrando valores iniciales para título del gráfico
     control = None
 
     for i in range(n_iter):
         if i == 40: # después de 40 generaciones se introduce el mecanismo de control
-            control = 'S'  # !!!!!!!!!!!! CAMBIAR A LA LETRA DEL MECANISMO DE CONTROL !!!!
+            control = 'M'  # !!!!! CAMBIAR A LA LETRA DEL MECANISMO DE CONTROL !!!!! -> 'C' castración, 'S' sacrificio, 'M' mixto
         I0, J0, A0, M0 = N_tn(I0, J0, A0, M0, control)
         I = np.append(I, I0)
         J = np.append(J, J0)
@@ -76,8 +76,8 @@ def graph(n_iter, I0, J0, A0, M0):
     plt.plot(M, label = 'Ancianos', marker='.')
     plt.legend(loc='center right', bbox_to_anchor=(1.30, 0.50), ncol=1, fancybox=True, shadow=False, fontsize=12, framealpha=0.0)
     plt.grid()
-    plt.title(f'Crecimiento poblacional - {control} - I({int(Io)}), J({int(Jo)}), A({int(Ao)}), M({int(Mo)})')
-    # plt.title(f'Crecimiento poblacional - I({int(Io)}), J({int(Jo)}), A({int(Ao)}), M({int(Mo)})')
+    plt.title(f'Crecimiento poblacional - {control} - I({int(Io)}), J({int(Jo)}), A({int(Ao)}), M({int(Mo)})') # título con mecanismo de control
+    # plt.title(f'Crecimiento poblacional - I({int(Io)}), J({int(Jo)}), A({int(Ao)}), M({int(Mo)})') # título de crecimiento poblacional sin mecanismo de control
     plt.xlabel('Años')
     plt.ylabel('Individuos')
     plt.savefig(f'Crecimiento_poblacional_control({control})_I{int(Io)}_J{int(Jo)}_A{int(Ao)}_M{int(Mo)}.png', transparent=False, bbox_inches='tight')
@@ -90,89 +90,4 @@ graph(100, 0, 0, 4, 0)
 
 
 
-
-
-
-
 # --------------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-#Ejemplo Jensen
-def N_tn(): # nit = número de iteraciones/generaciones
-    K = 110  # capacidad de carga
-    I = np.eye(4,4)
-
-    #Nt = np.vstack(np.array([75, 57, 35, 22, 11, 4, 4]))  # vector de distribución de edades
-    Nt = np.vstack(np.array([75, 57, 35, 22])) # vector de distribución de edades
-    N = np.sum(Nt)
-    '''
-    Leslie = np.array([[  0.426    , 1.290, 1.296, 1.120, 1.126, 1.554, 0], # matriz de Leslie
-                       [0.713, -1 , 0,0,0,0,0],
-                       [ 0, 0.645, -1, 0,0,0,0],
-                       [ 0,0,0.648,-1,0,0,0],
-                       [0,0,0,0.560,-1,0,0],
-                       [0,0,0,0,0.563,-1,0],
-                       [0,0,0,0,0,0.777,-1]])
-
-    '''
-    Leslie = np.array([[0.426, 1.290, 1.296, 1.120], # matriz de Leslie
-                       [0.713, -1 , 0,0],
-                       [ 0, 0.645, -1, 0],
-                       [ 0,0,0.648,-1]])
-
-    next_it = Nt+((K-N) / K) * np.dot((Leslie-I), (Nt))# solo para la siguiente generación
-
-    # intentando para varias generaciones
-    # for i in range(nit):
-    #     next_it = Nt + (((K-N) / K) * (Leslie - I) * (Nt))
-    #     Nt = next_it[:,2].astype(np.int32)
-    #     print(f'\ngeneracion {i}: \n{next_it}\n')
-
-    arr = np.around(next_it,0)# cogiendo le único vector de la matriz que da algún número
-    return arr
-
-def graph(n_iter):
-    #G1, G2, G3, G4, G5, G6, G7 = np.zeros(1), np.zeros(1), np.zeros(1), np.zeros(1), np.zeros(1), np.zeros(1), np.zeros(1)
-    G1, G2, G3, G4 = np.zeros(1), np.zeros(1), np.zeros(1), np.zeros(1)
-    '''
-    for i in range(n_iter):
-        G10, G20, G30, G40, G50, G60, G70 = N_tn()
-        G1 = np.append(G1, G10)
-        G2 = np.append(G2, G20)
-        G3 = np.append(G3, G30)
-        G4 = np.append(G4, G40)
-        G5 = np.append(G5, G50)
-        G6 = np.append(G6, G60)
-        G7 = np.append(G7, G70)
-    '''
-
-    for i in range(n_iter):
-        G10, G20, G30, G40 = N_tn()
-        G1 = np.append(G1, G10)
-        G2 = np.append(G2, G20)
-        G3 = np.append(G3, G30)
-        G4 = np.append(G4, G40)
-
-    '''
-    plt.plot(G1, label='1')
-    plt.plot(G2, label='2')
-    plt.plot(G3, label='3')
-    plt.plot(G4, label='4')
-    plt.plot(G5, label='5')
-    plt.plot(G6, label='6')
-    plt.plot(G7, label='7')
-    '''
-    plt.grid()
-    plt.plot(G1, label='1')
-    plt.plot(G2, label='2')
-    plt.plot(G3, label='3')
-    plt.plot(G4, label='4')
-    plt.legend()
-    plt.show()
-
-#graph(25)
