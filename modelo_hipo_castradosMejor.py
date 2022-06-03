d485d4bb55ed94e8dd34522b47f9c53829661488
Cesar Zapata <cesarzr10@gmail.com>
2022-06-02 19:51:08 -0500

    Funciona la castración separando adultos castrados y no-castrados, y se hace un subplot de 2x2 para mostrar los resultados. No está eficiente la parte de obtener cada resultado para los diferentes modelos de control

--- modelo_hipo_castradosMejor.py
+++ modelo_hipo_castradosMejor.py
@@ -1,8 +1,7 @@
-##
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+# para crecimiento poblacional sin control
 def N_tn(I0, J0, A0, M0, control):  # nit = número de iteraciones/generaciones
 
     castracion = (
@@ -57,9 +56,12 @@ def N_tn(I0, J0, A0, M0, control):  # nit = número de iteraciones/generaciones
     arr = np.around(next_it, 0)  # cogiendo el único vector de la matriz que da algún número
     return arr
 
-# función para hallar valores de la siguiente generación teniendo en cuenta adultos castrados y no-castrados
-def N_tn_cast(I0, J0, Ac0, A0, M0):  # nit = número de iteraciones/generaciones
 
+
+# función para hallar valores de la siguiente generación teniendo en cuenta adultos castrados y no-castrados
+def N_tn_cast(I0, J0, Ac0, A0, M0, mixto):  # nit = número de iteraciones/generaciones
+
+    sacrificio = mixto
 
     alpha = 1*0.5*0.8  # tasa de reproducción (1 por cada 0.5 mujeres y 0.8 adultos reprodutores)
     mI = 0.295 # prop NO supervivencia años 0-1
@@ -90,14 +92,29 @@ def N_tn_cast(I0, J0, Ac0, A0, M0):  # nit = número de iteraciones/generaciones
                        [ 0,        0    , pA,      pA   ,sM]])
     
 
+    # si se usa el sacrificio, se saca n número de hipopótamos de todas las categorías por año (cte)
+    if sacrificio:
+        next_it = Nt + ((K - N) / K) * np.dot((Leslie - I), (Nt))  # solo para la siguiente generación
+        arr = np.around(next_it, 0)  # cogiendo el único vector de la matriz que da algún número
+        final_arr = []  # array al que se le va a append el # de individuos de cada categoría después de sacrificar
+        n = 4  # número de individuos a sacrificar por cada categoría (total sacrificados=n*4)
+
+        for i in range(len(arr)):
+            final_arr.append(0) if arr[i] < n else final_arr.append(arr[i] - n) 
+            # se sacrifican n por generación, si hay menos de n entonces quedan 0 individuos
+
+        return final_arr
+
+
     next_it = Nt + ((K - N) / K) * np.dot((Leslie - I), (Nt))  # solo para la siguiente generación
     arr = np.around(next_it, 0)  # cogiendo el único vector de la matriz que da algún número
 
     return arr
 
 
+
 # función para graficar el crecimiento poblacional
-def graph(n_iter, I0, J0, A0, M0,control_met):
+def graph(n_iter, I0, J0, A0, M0, control_met):
     if control_met =='C':
         Ac0 = 0
         I, J, A, M = np.ones(1) * I0, np.ones(1) * J0, np.ones(1) * (A0 + Ac0), np.ones(1) * M0
@@ -107,13 +124,29 @@ def graph(n_iter, I0, J0, A0, M0,control_met):
         for i in range(n_iter):
             if i == 1:  # después de i generaciones se introduce el mecanismo de control
                 control = 'C'  # !!!!! CAMBIAR A LA LETRA DEL MECANISMO DE CONTROL !!!!! -> 'C' castración, 'S' sacrificio, 'M' mixto
-            I0, J0, Ac0, A0, M0 = N_tn_cast(I0, J0, Ac0, A0, M0)
+            I0, J0, Ac0, A0, M0 = N_tn_cast(I0, J0, Ac0, A0, M0, mixto=False)
             adultos = Ac0 + A0
             I = np.append(I, I0)
             J = np.append(J, J0)
             A = np.append(A, adultos)
             M = np.append(M, M0)
-            marcador = '.'
+
+    elif control_met == 'M':
+        Ac0 = 0
+        I, J, A, M = np.ones(1) * I0, np.ones(1) * J0, np.ones(1) * (A0 + Ac0), np.ones(1) * M0
+        Io, Jo, Aco, Ao, Mo = I0, J0, Ac0, A0, M0  # guadrando valores iniciales para título del gráfico
+        control = None
+
+        for i in range(n_iter):
+            if i == 1:  # después de i generaciones se introduce el mecanismo de control
+                control = 'M'  # !!!!! CAMBIAR A LA LETRA DEL MECANISMO DE CONTROL !!!!! -> 'C' castración, 'S' sacrificio, 'M' mixto
+            I0, J0, Ac0, A0, M0 = N_tn_cast(I0, J0, Ac0, A0, M0, mixto=True)
+            adultos = Ac0 + A0
+            I = np.append(I, I0)
+            J = np.append(J, J0)
+            A = np.append(A, adultos)
+            M = np.append(M, M0)
+
 
     elif control_met == '':
         I, J, A, M = np.ones(1) * I0, np.ones(1) * J0, np.ones(1) * A0, np.ones(1) * M0
@@ -128,7 +161,7 @@ def graph(n_iter, I0, J0, A0, M0,control_met):
             J = np.append(J, J0)
             A = np.append(A, A0)
             M = np.append(M, M0)
-            marcador = 's'
+            
     elif control_met == 'S':
 
         I, J, A, M = np.ones(1) * I0, np.ones(1) * J0, np.ones(1) * A0, np.ones(1) * M0
@@ -143,69 +176,74 @@ def graph(n_iter, I0, J0, A0, M0,control_met):
             J = np.append(J, J0)
             A = np.append(A, A0)
             M = np.append(M, M0)
-            marcador = '^'
 
-    plt.tight_layout()
+    return [I, J, A, M]
 
-    plt.plot(I, 'b',label="Infantes", marker=marcador, alpha=0.35)
-    plt.plot(J, 'r',label="Juveniles", marker=marcador, alpha=0.35)
-    plt.plot(A, 'g',label="Adultos",marker=marcador, alpha=0.35)
-    plt.plot(M, 'm', label="Mayores",marker=marcador, alpha=0.35)
-    plt.legend(loc='center right', bbox_to_anchor=(1.30, 0.50), ncol=1, fancybox=True, shadow=False, fontsize=9,
-               framealpha=0.0)
-    plt.grid()
-    plt.title(
-        f'Crecimiento poblacional - {control} - I({int(Io)}), J({int(Jo)}),  A({int(Ao)}), M({int(Mo)})')  # título con mecanismo de control
-    # plt.title(f'Crecimiento poblacional - I({int(Io)}), J({int(Jo)}), A({int(Ao)}), M({int(Mo)})') # título de crecimiento poblacional sin mecanismo de control
-    plt.xlabel('Años')
-    plt.ylabel('Individuos')
-    plt.savefig(f'Crecimiento_poblacional_control({control})_I{int(Io)}_J{int(Jo)}_A{int(Ao)}_M{int(Mo)}.png',
+
+# valores iniciales de cada categoría
+i, j, a, m = 2, 61, 39, 29
+
+# resultados para los diferentes modelos de control
+no_control = graph(50, i, j, a, m, '')
+castracion = graph(50, i, j, a, m, 'C')
+sacrificio = graph(50, i, j, a, m, 'S')
+mixto = graph(50, i, j, a, m, 'M')
+
+# subplot 2x2 de modelo sin control, castracion, sacrificio, y mixto
+figs, axs = plt.subplots(2, 2, figsize=(10,8), sharex=True)
+
+axs[0,0].plot(no_control[0], 'b',label="Infantes", marker='^', alpha=0.8)
+axs[0,0].plot(no_control[1], 'r',label="Juveniles", marker='^', alpha=0.8)
+axs[0,0].plot(no_control[2], 'g',label="Adultos",marker='^', alpha=0.8)
+axs[0,0].plot(no_control[3], 'm', label="Mayores",marker='^', alpha=0.8)
+axs[0,0].set_xlabel('Años')
+axs[0,0].set_ylabel('Individuos')
+axs[0,0].set_title('Sin control')
+axs[0,0].grid(alpha=0.3)
+
+
+axs[0,1].plot(castracion[0], 'b', marker='.', alpha=0.9)
+axs[0,1].plot(castracion[1], 'r', marker='.', alpha=0.9)
+axs[0,1].plot(castracion[2], 'g', marker='.', alpha=0.9)
+axs[0,1].plot(castracion[3], 'm', marker='.', alpha=0.9)
+axs[0,1].plot(no_control[0], 'b', marker='^', alpha=0.1)
+axs[0,1].plot(no_control[1], 'r', marker='^', alpha=0.1)
+axs[0,1].plot(no_control[2], 'g', marker='^', alpha=0.1)
+axs[0,1].plot(no_control[3], 'm', marker='^', alpha=0.1)
+axs[0,1].set_xlabel('Años')
+axs[0,1].set_ylabel('Individuos')
+axs[0,1].set_title('Castración')
+axs[0,1].grid(alpha=0.3)
+
+axs[1,0].plot(sacrificio[0], 'b', marker='.', alpha=0.9)
+axs[1,0].plot(sacrificio[1], 'r', marker='.', alpha=0.9)
+axs[1,0].plot(sacrificio[2], 'g', marker='.', alpha=0.9)
+axs[1,0].plot(sacrificio[3], 'm', marker='.', alpha=0.9)
+axs[1,0].plot(no_control[0], 'b', marker='^', alpha=0.1)
+axs[1,0].plot(no_control[1], 'r', marker='^', alpha=0.1)
+axs[1,0].plot(no_control[2], 'g', marker='^', alpha=0.1)
+axs[1,0].plot(no_control[3], 'm', marker='^', alpha=0.1)
+axs[1,0].set_xlabel('Años')
+axs[1,0].set_ylabel('Individuos')
+axs[1,0].set_title('Sacrificio')
+axs[1,0].grid(alpha=0.3)
+
+axs[1,1].plot(mixto[0], 'b', marker='.', alpha=0.9)
+axs[1,1].plot(mixto[1], 'r', marker='.', alpha=0.9)
+axs[1,1].plot(mixto[2], 'g', marker='.', alpha=0.9)
+axs[1,1].plot(mixto[3], 'm', marker='.', alpha=0.9)
+axs[1,1].plot(no_control[0], 'b', marker='^', alpha=0.1)
+axs[1,1].plot(no_control[1], 'r', marker='^', alpha=0.1)
+axs[1,1].plot(no_control[2], 'g', marker='^', alpha=0.1)
+axs[1,1].plot(no_control[3], 'm', marker='^', alpha=0.1)
+axs[1,1].set_xlabel('Años')
+axs[1,1].set_ylabel('Individuos')
+axs[1,1].set_title('Mixto')
+axs[1,1].grid(alpha=0.3)
+
+figs.tight_layout()
+figs.legend(loc='center', bbox_to_anchor=(1.1, 0.50), fancybox=True, shadow=False, fontsize=9,framealpha=0.0)
+
+plt.savefig(f'subplot_completo_I{int(i)}_J{int(j)}_A{int(a)}_M{int(m)}.png',
                 transparent=False, bbox_inches='tight')
-    plt.show()
-
-
-graph(80, 2, 61, 39, 29, '')  # -> 39 * 0.31 = 12.0
-
-
-
-
-
-"""
-def mostrar_menu() -> None:
-    print("\nMenú de opciones:")
-    print("1. Elegir parametros iniciales")
-    print("2. Elegir metodo de control")
-    print("3. Elija el numero de años para la simulación")
-    print("4. Ver grafica")
-    print("5. Salir")
-
-
-def iniciar_aplicacion() -> None:
-    print("Inicializando aplicacion")
-    continuar = True
-    metodo = ''
-    while continuar:
-        mostrar_menu()
-        opcion = int(input("Ingrese la opcion que desea ejecutar: "))
-        if opcion == 1:
-            Infantes = int(input("Ingrese el numero de infantes iniciales: "))
-            Juveniles = int(input("Ingrese el numero de Juveniles iniciales: "))
-            Adultos = int(input("Ingrese el numero de Adultos iniciales: "))
-            Mayores = int(input("Ingrese el numero de Mayores iniciales: "))
-        elif opcion == 2:
-            metodo = input("Ingrese el metodo de control adecuado: Castracion->(C) | Sin control->() | Sacrificio->(S)").strip()
-        elif opcion == 3:
-            años = int(input("Ingrese un numero de años para simular: "))
-        elif opcion == 4:
-            print(metodo)
-
-            graph(años, Infantes, Juveniles, Adultos, Mayores, metodo)
-        elif opcion == 5:
-            continuar = False
-        else:
-            print("Ingresó una opción no válida, por favor elija una de las opciones del menú")
-
-
-iniciar_aplicacion()
-
-"""
+plt.show()
